68dc1fb4804c9949c94683ac64c8f39fa144e849
Michael Richman <mr@bit.ly>
2015-07-08 22:38:28 -0600

    web server

---
+++ producer_web_server/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	//"github.com/bitly/go-nsq"
+)
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm() // parse arguments, you have to call this by yourself
+	fmt.Fprintf(w, "I would redirect you to \"%s\" if I were so inclined", r.Form["url"])
+}
+
+func main() {
+	http.HandleFunc("/redirect", redirect)
+	log.Printf("Listening...")
+	err := http.ListenAndServe(":9090", nil) // set listen port
+	if err != nil {
+		log.Fatal("Error starting server: ", err)
+	}
+}
